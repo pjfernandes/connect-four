0d4d433d5aa30a100f3ecf45585d550ea28b121c
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-04-27 00:35:02 -0300

    diagonal win? method implemented

--- connect-four.rb
+++ connect-four.rb
@@ -16,7 +16,7 @@ class ConnectFour
   def game
     puts "GAME STARTED!"
 
-    until player1_row_win? || player2_row_win? || player1_col_win? || player2_col_win?
+    until player1_row_win? || player2_row_win? || player1_col_win? || player2_col_win? || player1_diagonal_win? || player2_diagonal_win?
       player1_move
       player2_move
     end
@@ -28,6 +28,7 @@ class ConnectFour
     display_board
     print "COL: "
     col = gets.chomp.to_i
+
     if valid_move?(col) == false
       puts "INVALID MOVE"
       until valid_move?(col)
@@ -59,7 +60,7 @@ class ConnectFour
    def player2_move
       display_board
       print "COL: "
-      col = gets.chomp.to_i
+      col = gets.chomp
       if valid_move?(col) == false
         puts "INVALID MOVE"
         until valid_move?(col)
@@ -117,7 +118,7 @@ class ConnectFour
     result
   end
 
-  def player2_row_win?
+  def player1_row_win?
     result = nil
     for row in 0...6
       row_matrix = @board.row(row).to_a
@@ -147,8 +148,218 @@ class ConnectFour
     result
   end
 
+  def player1_diagonal_win?
+    sub_matrices =
+      [
+        [
+          @board[0,3],
+          @board[1,2],
+          @board[2,1],
+          @board[3,0]
+        ],
+        [
+          @board[0,4],
+          @board[1,3],
+          @board[2,2],
+          @board[3,1],
+          @board[4,0]
+        ],
+        [ @board[0,5],
+          @board[1,4],
+          @board[2,3],
+          @board[3,2],
+          @board[4,1],
+          @board[5,0]
+        ],
+        [ @board[0,5],
+          @board[1,4],
+          @board[2,3],
+          @board[3,2],
+          @board[4,1],
+          @board[5,0]
+        ],
+        [
+          @board[0,6],
+          @board[1,5],
+          @board[2,4],
+          @board[3,3],
+          @board[4,2],
+          @board[5,1]
+        ],
+        [
+          @board[1,6],
+          @board[2,5],
+          @board[3,4],
+          @board[4,3],
+          @board[5,2]
+        ],
+        [
+          @board[2,6],
+          @board[3,5],
+          @board[4,4],
+          @board[5,3]
+        ],
+        [
+          @board[2,0],
+          @board[3,1],
+          @board[4,2],
+          @board[5,3],
+        ],
+        [
+          @board[1,0],
+          @board[2,1],
+          @board[3,2],
+          @board[4,3],
+          @board[5,4]
+        ],
+        [
+          @board[0,0],
+          @board[1,1],
+          @board[2,2],
+          @board[3,3],
+          @board[4,4],
+          @board[5,5]
+        ],
+        [
+          @board[0,1],
+          @board[1,2],
+          @board[2,3],
+          @board[3,4],
+          @board[4,5],
+          @board[5,6]
+        ],
+        [
+          @board[0,2],
+          @board[1,3],
+          @board[2,4],
+          @board[3,5],
+          @board[4,6]
+        ],
+        [
+          @board[0,3],
+          @board[1,4],
+          @board[2,5],
+          @board[3,6]
+        ]
+      ]
+
+    result_sub = ["X","X","X","X"]
+    results = []
+
+    sub_matrices.each do |matrix|
+      results << matrix.each_cons(result_sub.size).any?(&result_sub.method(:==))
+    end
+
+    results.any?(true)
+  end
+
+  def player2_diagonal_win?
+    sub_matrices =
+      [
+        [
+          @board[0,3],
+          @board[1,2],
+          @board[2,1],
+          @board[3,0]
+        ],
+        [
+          @board[0,4],
+          @board[1,3],
+          @board[2,2],
+          @board[3,1],
+          @board[4,0]
+        ],
+        [ @board[0,5],
+          @board[1,4],
+          @board[2,3],
+          @board[3,2],
+          @board[4,1],
+          @board[5,0]
+        ],
+        [ @board[0,5],
+          @board[1,4],
+          @board[2,3],
+          @board[3,2],
+          @board[4,1],
+          @board[5,0]
+        ],
+        [
+          @board[0,6],
+          @board[1,5],
+          @board[2,4],
+          @board[3,3],
+          @board[4,2],
+          @board[5,1]
+        ],
+        [
+          @board[1,6],
+          @board[2,5],
+          @board[3,4],
+          @board[4,3],
+          @board[5,2]
+        ],
+        [
+          @board[2,6],
+          @board[3,5],
+          @board[4,4],
+          @board[5,3]
+        ],
+        [
+          @board[2,0],
+          @board[3,1],
+          @board[4,2],
+          @board[5,3],
+        ],
+        [
+          @board[1,0],
+          @board[2,1],
+          @board[3,2],
+          @board[4,3],
+          @board[5,4]
+        ],
+        [
+          @board[0,0],
+          @board[1,1],
+          @board[2,2],
+          @board[3,3],
+          @board[4,4],
+          @board[5,5]
+        ],
+        [
+          @board[0,1],
+          @board[1,2],
+          @board[2,3],
+          @board[3,4],
+          @board[4,5],
+          @board[5,6]
+        ],
+        [
+          @board[0,2],
+          @board[1,3],
+          @board[2,4],
+          @board[3,5],
+          @board[4,6]
+        ],
+        [
+          @board[0,3],
+          @board[1,4],
+          @board[2,5],
+          @board[3,6]
+        ]
+      ]
+
+    result_sub = ["O","O","O","O"]
+    results = []
+
+    sub_matrices.each do |matrix|
+      results << matrix.each_cons(result_sub.size).any?(&result_sub.method(:==))
+    end
+
+    results.any?(true)
+  end
+
   def valid_move?(input)
-    (input.to_i.class == Integer) && (input >= 0 && input < 6) ? true : false
+    (input.class == Integer) && (input >= 0 && input < 6) ? true : false
   end
 
   def display_board
